46606d5aba80610fe50648270fe25fe006e056df
santoshpal <santoshpal61437@gmail.com>
2023-08-02 05:07:52 -0700

    ui changes

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,10 +2,13 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { ENDPOINTS } from '../Api/Endpoints';
 import '.././App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const Weather = () => {
-    const [city, setCity] = useState('Indore');
+    const [city, setCity] = useState('Bengaluru');
     const [weatherData, newWetherData] = useState([]);
     const [error, setError] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -18,11 +21,12 @@ const Weather = () => {
         try {
             setLoading(true);
             const response = await axios.get(`${ENDPOINTS.ApiBaseUrl}${city}&units=metric&appid=${ENDPOINTS.WeatherKey}`);
-            // console.log('weather appp....', response.data);
+            console.log('weather appp....', response.data);
             newWetherData(response.data);
             setLoading(false);
         } catch (error) {
             setError(error.response.data.message);
+            toast.error(error.response.data.message);
             // console.log('401 error ......',error.response.data.message)
             setLoading(false);
         }
@@ -30,97 +34,88 @@ const Weather = () => {
 
     return (
         <>
-            <div className="container">
-                <div className="text-center py-3">{loading ?
-                    <div className="spinner-border text-primary" role="status">
-                        <span className="sr-only"></span>
-                    </div> : null}
-                </div>
+            <ToastContainer />
+            <div className="container-fluid">
+                <div className="container py-5">
+                    <div className="text-center">{loading ?
+                        <div className="spinner-border text-white" role="status">
+                            <span className="sr-only"></span>
+                        </div> : null}
+                    </div>
 
-                {error && <div className="error_messge">{error}
-
-                </div>}
-
-                <h1 className="heading">Weather App</h1>
-                <input
-                    className="form-control mr-sm-2"
-                    type="text"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    placeholder="Enter city name"
-                />
-                <br />
-                <div className="text-center">
-                    <button className="btn btn-primary btn-md" onClick={getAllweather}>Search City </button>
-                </div>
-                <br />
-
-
-                {/* display weather */}
-
-                {Object.keys(weatherData).length > 0 &&
-                    <div className="col-md-12 text-center">
-
-                        <div className="shadow rounded wetherResultBox">
-                            <img className="weathorIcon"
-                                src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
-
-                            <h5 className="weathorCity">{weatherData?.name} </h5>
-
-                            <h6 className="weatherText"> Temperature {((weatherData?.main?.temp) - 273.15).toFixed(2)}°C</h6>
-
-                            <div class="d-flex flex-row py-3">
-                                <div class="p-2">
-                                    <h6 className="weathorTemp"> Feels's Like {((weatherData?.main?.feels_like) - 273.15).toFixed(2)}°C</h6>
+                    <div className="row gx-5">
+                        <div className="col-md-4 col-lg-4 col-12 card text-center search_left">
+                            <div className="py-5 mb-5">
+                                <h1 className="">Weather by city</h1>
+                                <p className="">Please enter the city name</p>
+                                <div className="py-3">
+                                    <input
+                                        className="form-control mr-sm-2"
+                                        type="text"
+                                        value={city}
+                                        onChange={(e) => setCity(e.target.value)}
+                                        placeholder="Enter city name"
+                                    />
                                 </div>
-                                <div class="p-2">
-                                    <h6 className="weathorTemp"> Pressure : {((weatherData?.main?.pressure))}°C</h6>
+                                <div className="py-2">
+                                    <button className="btn btn-primary btn-md btn-block" onClick={getAllweather}>Get Weather </button>
                                 </div>
-                                <div class="p-2">
-                                    <h6 className="weathorTemp"> Temp Max : {((weatherData?.main?.temp_max)).toFixed(2)}°C</h6>
-                                </div>
-
-                                <div class="p-2">
-                                    <h6 className="weathorTemp"> Temp Min : {((weatherData?.main?.temp_max)).toFixed(2)}°C</h6>
-                                </div>
-
-                            </div>
-
-
-                            <div class="d-flex flex-row-reverse mx-5">
-
-                                <div class="p-2">
-                                    <h3 className="weathorTemp"> Humidity : {((weatherData?.main?.humidity) - 273.15).toFixed(2)}°C</h3>
-                                </div>
-
-                                <div class="p-2">
-                                    <h3 className="weathorTemp"> Degree : {((weatherData?.wind?.deg) - 273.15).toFixed(2)}°C</h3>
-                                </div>
-
-                                <div class="p-2">
-
-                                    <h3 className="weathorTemp"> Speed : {((weatherData?.wind?.speed) - 273.15).toFixed(2)}°C</h3>
-                                </div>
-                            </div>
-
-                            <div className="text-center">
-
-                                {
-                                    weatherData.weather.map((item, index) => {
-                                        return (
-                                            <div className="py-3" key={index}>
-                                                <h6 className="weathorCity">{item.main}</h6>
-                                                <h3 className="text-primary font-weight-bold"> Description :{item.description}</h3>
-                                            </div>
-                                        );
-                                    })
-                                }
-
                             </div>
                         </div>
-                    </div>
-                }
 
+                        {
+                            Object.keys(weatherData).length > 0 &&
+                        <div className="col-md-7 col-lg-7 col-12 text-center card  mx-5">
+                            <div class="card-body head city_Section">
+                                <div class="card-body center right text-center">
+                                {
+                                        weatherData.weather.map((item, index) => {
+                                            return (
+                                                <div className="py-3" key={index}>
+                                                    <h6 className="weathorCity">{item.main}</h6>
+                                                 {/* <h3 className="weathorCity font-weight-bold"> Description :{item.description}</h3>  */}
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                    <img alt="" className="img-fluid img-responsive" src="http://openweathermap.org/img/w/04d.png" />
+                                    <h1 class="card-text">{((weatherData?.main?.pressure) - 273.15).toFixed(2)} ℉</h1>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <h5 class="card-title">Weather Details for city : {weatherData?.name}</h5>
+                                <table class="table py-2">
+                                    <tbody>
+                                        <tr>
+                                            <td>Temperature </td>
+                                            <td>:{((weatherData?.main?.temp) - 273.15).toFixed(2)} ℉</td>
+
+                                            <td>Temperature Min </td>
+                                            <td>:{((weatherData?.main?.temp_min) - 273.15).toFixed(2)} ℉</td>
+                                        </tr>
+                                        <tr>
+                                            <td >Feels like </td>
+                                            <td>:{((weatherData?.main?.feels_like) - 273.15).toFixed(2)}°F </td>
+                                            <td >Pressure </td>
+                                            <td>:{((weatherData?.main?.pressure))}°C</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Himudity </td>
+                                            <td>: {((weatherData?.main?.humidity) - 273.15).toFixed(2)}</td>
+                                            <td>Wind </td>
+                                            <td>: {((weatherData?.wind?.speed))} Mph</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+
+                        }
+
+                    </div>
+                </div>
             </div>
 
         </>
